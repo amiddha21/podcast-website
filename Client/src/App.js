c8ca8dde95eccfbd862c019e88232d6a6bd65cd7
amiddha <amiddha@umich.edu>
2020-08-24 12:30:08 -0400

    initial commit

---
+++ Client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ContextState from './context_state_config';
+//import NavBar from './components/HeaderComponent/NavBar';
+//import { Button } from 'semantic-ui-react'
+
+const App = () => {
+    return (
+        <div>
+          React
+          <ContextState/>
+        </div>
+    );
+}
+
+export default App;
---
+++ Client/src/hooks/header.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import Context from '../utils/context';
+
+const Header = () => {
+  const context = useContext(Context)
+
+    return(
+        <div>
+          <Link to='/' style={{padding: '5px'}}>
+            Home
+          </Link>
+          <Link to='/profile' style={{padding: '5px'}}>
+            Profile
+          </Link>
+          <Link to='/hooksform' style={{padding: '5px'}}>
+            Temp Form
+          </Link>
+          <Link to='/hookscontainer' style={{padding: '5px'}}>
+            Temp Container
+          </Link>
+          <Link to='/privateroute' style={{padding: '5px'}}>
+            Private Route
+          </Link>
+          <Link to='/posts' style={{padding: '5px'}}>
+             Posts
+          </Link>
+          {!context.authState
+            ? <button onClick={() => context.authObj.login()}>Login</button>
+            : <button onClick={() => context.authObj.logout()}>Logout</button>
+          }
+        </div>
+  )};
+
+
+export default Header;
